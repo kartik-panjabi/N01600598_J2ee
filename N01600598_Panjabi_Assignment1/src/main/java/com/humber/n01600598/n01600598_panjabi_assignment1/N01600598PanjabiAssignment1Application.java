f8652b9db7272ef0b4fdfda89d504a3697968c28
kartik-panjabi <82868772+kartik-panjabi@users.noreply.github.com>
2025-02-03 13:56:59 -0500

    Assignment 1 added

---
+++ N01600598_Panjabi_Assignment1/src/main/java/com/humber/n01600598/n01600598_panjabi_assignment1/N01600598PanjabiAssignment1Application.java
@@ -0,0 +1,13 @@
+package com.humber.n01600598.n01600598_panjabi_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N01600598PanjabiAssignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N01600598PanjabiAssignment1Application.class, args);
+    }
+
+}
