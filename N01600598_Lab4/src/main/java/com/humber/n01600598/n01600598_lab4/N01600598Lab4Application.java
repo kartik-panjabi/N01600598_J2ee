63b702267b69988096c759da07731072b25c177d
kartik-panjabi <82868772+kartik-panjabi@users.noreply.github.com>
2025-02-05 16:20:39 -0500

    updated

---
+++ N01600598_Lab4/src/main/java/com/humber/n01600598/n01600598_lab4/N01600598Lab4Application.java
@@ -0,0 +1,13 @@
+package com.humber.n01600598.n01600598_lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N01600598Lab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N01600598Lab4Application.class, args);
+    }
+
+}
---
+++ N01600598_Lab4/src/test/java/com/humber/n01600598/n01600598_lab4/N01600598Lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.n01600598.n01600598_lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N01600598Lab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
