8a7edadc20111c1cc891c174a98c06c1963cb11e
kartik-panjabi <82868772+kartik-panjabi@users.noreply.github.com>
2025-02-05 16:04:29 -0500

    updated

---
+++ N01600598_Lab4/src/main/java/com/humber/n01600598/n01600598_lab4/entity/Employee.java
@@ -0,0 +1,7 @@
+package com.humber.n01600598.n01600598_lab4.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+public class Employee {
+}
---
+++ N01600598_Lab4/src/main/java/com/humber/n01600598/n01600598_lab4/service/EmployeeServiceImpl.java
@@ -0,0 +1,4 @@
+package com.humber.n01600598.n01600598_lab4.service;
+
+public class EmployeeServiceImpl {
+}
