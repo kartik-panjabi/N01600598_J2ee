39b2c9c11f84b8cce6ef8263ab6b05c2a2dea041
kartik-panjabi <82868772+kartik-panjabi@users.noreply.github.com>
2025-03-10 16:05:05 -0400

    lombok fixed

--- N01600598_Panjabi_Test3_IMS/src/main/java/com/humber/n01600598/entity/Product.java
+++ N01600598_Panjabi_Test3_IMS/src/main/java/com/humber/n01600598/entity/Product.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Product {
 
     @Id
@@ -18,44 +20,4 @@ public class Product {
     private double price;
     private int stock;
 
-    public Product() {}
-    public Product(int id, String name, String description, double price, int stock) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.stock = stock;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-
-    }
-    public String getName() {
-        return name;
-
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    public int getStock() {
-        return stock;
-    }
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 }
