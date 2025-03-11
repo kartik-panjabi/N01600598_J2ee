864c382acb8c25bbac385832b2a5a10c59655957
kartik-panjabi <82868772+kartik-panjabi@users.noreply.github.com>
2025-03-11 10:32:27 -0400

    week 8 rest api added

---
+++ Kartik_week8/src/main/java/com/humber/n01600598/KartikWeek8Application.java
@@ -0,0 +1,13 @@
+package com.humber.n01600598;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartikWeek8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KartikWeek8Application.class, args);
+    }
+
+}
---
+++ Kartik_week8/src/test/java/com/humber/n01600598/KartikWeek8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.n01600598;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartikWeek8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
